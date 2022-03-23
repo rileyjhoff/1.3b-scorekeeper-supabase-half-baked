f1168b30144053d1ac3d1c8d33600f297997573a
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-23 12:56:58 -0700

    finished displayCurrentGameEl event listeners and all increment/decerement event listeners. current game is now working as attending

--- games/games.js
+++ games/games.js
@@ -42,33 +42,33 @@ nameForm.addEventListener('submit', (e) => {
 
 teamOneAddButton.addEventListener('click', () => {
     // increment the current state for team one's score
-    
+    score1++;
     displayCurrentGameEl();
 });
 
 teamTwoAddButton.addEventListener('click', () => {
     // increment the current state for team two's score
-
+    score2++;
     displayCurrentGameEl();
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
     // decrement the current state for team one's score
-
+    score1--;
     displayCurrentGameEl();
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
     // decrement the current state for team two's score
-
+    score2--;
     displayCurrentGameEl();
 });
 
 finishGameButton.addEventListener('click', async() => {
     // create a new game using the current game state
-    
+    const addGame = createGame(game);
     // after creating this new game, re-fetch the games to get the updated state and display them (hint: call displayAllGames())
-            
+    displayAllGames();
     name1 = '';
     name2 = '';
     score1 = 0;
@@ -82,30 +82,37 @@ logoutButton.addEventListener('click', () => {
 });
 
  // on load . . .
-window.addEventListener('', async() => {
+window.addEventListener('load', async() => {
     // display all past games (hint: call displayAllGames())
 });
 
 
 function displayCurrentGameEl() {
     // clear out the current game div
-    
+    currentGameEl.textContent = '';
     // change the label to show team one's name;
     // change the label to show team two's name;
-
+    teamOneLabel.textContent = name1;
+    teamTwoLabel.textContent = name2;
     // call the render game function to create a game element
-    
+    const game = { name1:name1, name2:name2, score1:score1, score2:score2 };
+    const newGame = renderGame(game);
     // append the element to the cleared out current game div
+    currentGameEl.append(newGame);
 }
 
 
 function displayAllGames() {
     // clear out the past games list in the DOM
-    
+    pastGamesEl.textContent = '';
     // FETCH ALL GAMES from supabase
-
+    const allGames = getGames();
     // loop through the past games 
     // render and append a past game for each past game in state
+    for (let game of allGames) {
+        const gameDiv = renderGame(game);
+        pastGamesEl.append(gameDiv);
+    }
 }
 
 
